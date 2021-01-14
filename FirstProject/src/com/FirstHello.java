10976d93f3607e5a86a93b5f2b61c3d749d47c99
manish-zensar-trg <76413145+manish-zensar-trg@users.noreply.github.com>
2021-01-14 13:47:47 +0530

    added for loop in main method

--- FirstProject/src/com/FirstHello.java
+++ FirstProject/src/com/FirstHello.java
@@ -3,7 +3,9 @@ package com;
 public class FirstHello {
 
 	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
 		System.out.println("First Hello");
+		}
 	}
 	
 	public static void message() {
