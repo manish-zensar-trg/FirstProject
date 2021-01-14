f7b83f00e32ae5b62c55a795e3e1d2eb7d467dda
manish-zensar-trg <76413145+manish-zensar-trg@users.noreply.github.com>
2021-01-14 13:44:01 +0530

    first commit

---
+++ FirstProject/src/com/FirstHello.java
@@ -0,0 +1,13 @@
+package com;
+
+public class FirstHello {
+
+	public static void main(String[] args) {
+		System.out.println("First Hello");
+	}
+	
+	public static void message() {
+		System.out.println("This is another message");		
+	}
+
+}
